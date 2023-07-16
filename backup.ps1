29c020bbc58e63a101c2b61a55e136a066cb0062
Tri || Hydera <39857764+TriHydera@users.noreply.github.com>
2023-07-16 20:56:41 +0100

    Update backup.ps1
    - Now grabs the source computer name from system
    - Added override mode with prompts
    - Shows default values in the prompts

--- backup.ps1
+++ backup.ps1
@@ -1,10 +1,32 @@
+# Welcome text
+Write-Host "Avilable on Github: https://github.com/ChrisTitusTech/powershell-scripts"
+
 # Backup source computer
-$sourceComputer = "COMPUTER_NAME" # Replace with the name of the source computer
+$sourceComputer = $env:COMPUTERNAME # Grabs the name of the source computer
 $backupLocation = "C:\Backup" # Replace with the desired backup location on the source computer
 
 # Backup destination computer
 $destinationComputer = "COMPUTER_NAME" # Replace with the name of the destination computer
 $restoreLocation = "C:\Restore" # Replace with the desired restore location on the destination computer
+
+# Prompts
+$Mode = Read-Host "Manual input? (Y/n)"
+if($Mode -eq "Y"){
+    Read-Host -Prompt "Source Computer Name ($sourceComputer)" -OutVariable $tmp
+    if($tmp -ne $null){
+        $sourceComputer = $tmp
+    } else {
+        Write-Host "No input entered, exiting"
+        Exit
+    }
+    Read-Host -Prompt "Destination Computer Name ($destinationComputer)" -OutVariable $tmp
+    if($tmp -ne $null){
+        $destinationComputer = $tmp
+    } else {
+        Write-Host "No input entered, exiting"
+        Exit
+    }
+}
 
 # Backup the source computer
 Write-Host "Backing up $sourceComputer..."
@@ -14,5 +36,5 @@ Start-Process -Wait -NoNewWindow -FilePath "wbadmin" -ArgumentList "start backup
 Write-Host "Restoring to $destinationComputer..."
 Start-Process -Wait -NoNewWindow -FilePath "wbadmin" -ArgumentList "start recovery -backupTarget:`"$backupLocation`" -machineName:`"$destinationComputer`" -recoveryTarget:`"$restoreLocation`" -quiet"
 
+# End message
 Write-Host "Backup and restore completed successfully."
-
