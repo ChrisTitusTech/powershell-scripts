1845c3d20abb488f5e3ea79dcc1f8b8b1f7300e1
Chris Titus <dfm.titus@gmail.com>
2023-07-17 21:19:54 -0500

    add

---
+++ bckp_windows_drivers.ps1
@@ -0,0 +1,17 @@
+﻿# backup current configuration for drivers services
+$filename = "$env:USERPROFILE\Desktop\Backup_Drivers-Services_Current-State.reg"
+$regHeader = 'Windows Registry Editor Version 5.00'
+
+Set-Content $filename $regHeader
+
+driverquery /FO CSV | ForEach-Object {
+    $svc = $_.Split(",")[0].Trim('"')
+    $start = (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\$svc" -Name Start -ErrorAction SilentlyContinue).'Start'
+    if ($start -match "[0-4]$") {
+        $start = [int]$start
+        $regKey = "[$('HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\'+$svc)]"
+        $regValue = "`"Start`"=dword:0000000$start"
+        $regEntry = "$regKey`n$regValue`n$regDelimiter"
+        Add-Content $filename $regEntry
+    }
+} > $null 2>&1
---
+++ bckp_windows_services.ps1
@@ -0,0 +1,12 @@
+﻿# backup current configuration for windows services
+$filename = "$env:USERPROFILE\Desktop\Backup_Windows-Services_Current-State.reg"
+"Windows Registry Editor Version 5.00" | Out-File $filename -Encoding ASCII
+Get-Service | Where-Object {$_.Name -cmatch "[a-z]" -and $_.Name -ne "TermService"} | ForEach-Object {
+    $svc = $_.Name
+    $start = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\$svc" -Name Start -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Start
+    if ($start -match "[0-4]$") {
+        $start = [int]$start
+        "`n[$('HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\'+$svc)]" | Out-File $filename -Encoding ASCII -Append
+        "`"Start`"=dword:0000000$($start)" | Out-File $filename -Encoding ASCII -Append
+    }
+} > $null
